fbfb4c6e768cb58c00e1c8f088fbda5e3556833d
jgiors <jgiors@yahoo.com>
2024-07-14 22:38:50 -0700

    Demonstrate macros controlling rather complex expressions, in this case, writes to cout. It isn't too surprising that this works, since macros operate on text, and expressions are expected to be able to be used as arguments.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 
 using std::cout;
 
+#define VERBOSE_ENABLED(X)  (X)
+#define VERBOSE_DISABLED(...)
+
 int main(int argc, char* argv[])
 {
-    cout << "\nHowdy!\n\n";
+    int answer = 42;
+    VERBOSE_ENABLED(cout << "\nHowdy! The answer is " << answer << ". I kid you not.\n\n");
+    VERBOSE_DISABLED(cout << "*** THIS SHOULD NOT PRINT, NOT EVEN THE ANSWER " << answer << " ***\n\n");
     return 0;
 }
