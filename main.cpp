79b5c8ac597ad6ebaf36cd3d28c9d86d50cb8e21
jgiors <jgiors@yahoo.com>
2024-07-14 23:05:30 -0700

    example of logging using a stream wrapper

--- main.cpp
+++ main.cpp
@@ -5,10 +5,22 @@ using std::cerr;
 #define VERBOSE_ENABLED(X)  (X)
 #define VERBOSE_DISABLED(...)
 
+class Log {
+public:
+    template <typename T>
+    Log& operator<<(T x)
+    {
+        cerr << x;
+        return *this;
+    }
+};
+
+Log logger;
+
 int main(int argc, char* argv[])
 {
     int answer = 42;
-    VERBOSE_ENABLED(cerr << "\nHowdy! The answer is " << answer << ". I kid you not.\n\n");
-    VERBOSE_DISABLED(cerr << "*** THIS SHOULD NOT PRINT, NOT EVEN THE ANSWER " << answer << " ***\n\n");
+    VERBOSE_ENABLED(logger << "\nHowdy! The answer is " << answer << ". I kid you not.\n\n");
+    VERBOSE_DISABLED(logger << "*** THIS SHOULD NOT PRINT, NOT EVEN THE ANSWER " << answer << " ***\n\n");
     return 0;
 }
