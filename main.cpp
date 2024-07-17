f45c1d7817a2c5589b95f583bd26b6e8600200c3
jgiors <jgiors@yahoo.com>
2024-07-16 23:39:09 -0700

    This is better, but still seems a bit convoluted.

--- main.cpp
+++ main.cpp
@@ -3,43 +3,28 @@
 #include <tuple>
 #include <utility>
 
-/*
 template <typename... T>
-struct TupleContainer
+struct TuplePrinter
 {
     std::tuple<T...> _tuple;
-    TupleContainer(T... args) : _tuple(args...) {}
-};
+    TuplePrinter(T... args) : _tuple(args...) {}
 
-template <typename T, typename... Args>
-void PrintTuple(T t, Args... args)
-{
-    cout << t;
-    PrintTuple(args...);
-}
-*/
-
-auto _tuple{std::make_tuple( "Hi", 42 )};
-
-template<int N>
-void printTuple()
-{
-    if (N < std::tuple_size<decltype(_tuple)>::value)
+    template<int N>
+    void print()
     {
-        std::cout << std::get<N>(_tuple);
-        printTuple<N + 1>();
+        print<N - 1>();
+        std::cout << std::get<N - 1>(_tuple) << std::endl;
     }
-}
 
-template <>
-void printTuple<2>()
-{
-}
+    template<>
+    void print<0>() {}
+
+    void print() { print<std::tuple_size<std::tuple<T...>>::value>(); }
+};
 
 int main(int argc, char* argv[])
 {
-//    TupleContainer<std::string, int> container{ "Hi", 42 };
-//    std::apply(PrintTuple, container._tuple);
-    printTuple<0>();
+    TuplePrinter<std::string, int, std::string> tp( "Hi", 42, "Duh!" );
+    tp.print();
     return 0;
 }
