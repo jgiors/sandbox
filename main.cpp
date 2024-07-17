3911bc06c4e72abfa471c86561f28dd0b6ef2efe
jgiors <jgiors@yahoo.com>
2024-07-16 22:57:45 -0700

    OK, so this works, but it's really clunky.

--- main.cpp
+++ main.cpp
@@ -1,31 +1,45 @@
 #include <Windows.h>
-#include <sstream>
 #include <iostream>
+#include <tuple>
+#include <utility>
 
-using std::cerr;
-
-#define VERBOSE_ENABLED(X)  (X)
-#define VERBOSE_DISABLED(...)
-
-class Logger {
-public:
-    template <typename T>
-    Logger& operator<<(const T &x)
-    {
-        std::ostringstream s;
-        s << x;
-        cerr << s.str();
-        OutputDebugString(s.str().c_str());
-        return *this;
-    }
+/*
+template <typename... T>
+struct TupleContainer
+{
+    std::tuple<T...> _tuple;
+    TupleContainer(T... args) : _tuple(args...) {}
 };
 
-Logger Log;
+template <typename T, typename... Args>
+void PrintTuple(T t, Args... args)
+{
+    cout << t;
+    PrintTuple(args...);
+}
+*/
+
+auto _tuple{std::make_tuple( "Hi", 42 )};
+
+template<int N>
+void printTuple()
+{
+    if (N < std::tuple_size<decltype(_tuple)>::value)
+    {
+        std::cout << std::get<N>(_tuple);
+        printTuple<N + 1>();
+    }
+}
+
+template <>
+void printTuple<2>()
+{
+}
 
 int main(int argc, char* argv[])
 {
-    int answer = 42;
-    VERBOSE_ENABLED(Log << "\nHowdy! The answer is " << answer << ". I kid you not.\n\n");
-    VERBOSE_DISABLED(Log << "*** THIS SHOULD NOT PRINT, NOT EVEN THE ANSWER " << answer << " ***\n\n");
+//    TupleContainer<std::string, int> container{ "Hi", 42 };
+//    std::apply(PrintTuple, container._tuple);
+    printTuple<0>();
     return 0;
 }
